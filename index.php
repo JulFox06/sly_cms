879d2e164c763bf7c15bafb52219c8b945ed4436
JulFox06 <renard.julien55@gmail.com>
2017-02-24 21:23:49 +0100

    Reset CMS file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$configfile = "contents/sly_config.php";
-if(file_exists($configfile) AND filesize($configfile) > 0) {
-	header("Location:web/");
-}
-else {
-	header("Location:install/");
-}
-?>
--- web/admin/delete/delete_droit.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../../contents/sly_config.php';
-$req = "UPDATE sly_user SET groupe='Visiteur' WHERE uid = ".$_POST['id'];
-$res = mysqli_query($lien, $req);
-if (!$res) {
-	echo "Erreur SQL : $req <br>".mysqli_error($lien);
-}
