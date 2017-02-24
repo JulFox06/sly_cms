702d01dde0dc45273318395a043a5b8c05297240
JulFox06 <renard.julien55@gmail.com>
2017-02-24 21:30:34 +0100

    Remove all file

--- web/admin/delete/delete_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../../contents/sly_config.php';
-$req = "DELETE FROM sly_user WHERE uid = ".$_POST['id'];
-$res = mysqli_query($lien, $req);
-if (!$res) {
-	echo "Erreur SQL : $req <br>".mysqli_error($lien);
-}
